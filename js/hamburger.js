283491caabdc068cb4bf879ddaf792f1a45abb01
balaji1832 <balaji49034@gmail.com>
2026-05-22 11:58:00 +0530

    Header Updated

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,11 +1,22 @@
+  const menuBtn = document.getElementById("menuBtn");
+  const mobileMenu = document.getElementById("mobileMenu");
+  const menuIcon = document.getElementById("menuIcon");
+  const closeIcon = document.getElementById("closeIcon");
 
-    const menuBtn = document.getElementById("menuBtn");
-    const mobileMenu = document.getElementById("mobileMenu");
-    const menuIcon = document.getElementById("menuIcon");
-    const closeIcon = document.getElementById("closeIcon");
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+    menuIcon.classList.toggle("hidden");
+    closeIcon.classList.toggle("hidden");
+  });
 
-    menuBtn.addEventListener("click", () => {
-      mobileMenu.classList.toggle("hidden");
-      menuIcon.classList.toggle("hidden");
-      closeIcon.classList.toggle("hidden");
+  const dropdownButtons = document.querySelectorAll(".mobileDropdownBtn");
+
+  dropdownButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const dropdown = button.nextElementSibling;
+      const arrow = button.querySelector(".dropdownArrow");
+
+      dropdown.classList.toggle("hidden");
+      arrow.classList.toggle("rotate-180");
     });
+  });
